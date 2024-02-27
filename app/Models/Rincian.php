d9009f541f8c7c0d74f6eb4a1c36b026b6c09340
asrani <asrani.27@gmail.com>
2024-02-27 13:24:06 +0800

    f

--- app/Models/Rincian.php
+++ app/Models/Rincian.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Rincian extends Model
 {
     use HasFactory;
-    protected $table = 'npd_rincian';
+    protected $table = 'rincian';
     protected $guarded = ['id'];
     public $timestamps = false;
 
